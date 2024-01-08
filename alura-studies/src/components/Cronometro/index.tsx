571ebf6d00bd7fe6d75c205de43b270e2246da84
RaulAnselmoDSantos <raul.trabalho@gmail.com>
2024-01-08 18:57:08 -0300

    feat: fiz the start and stop button

--- alura-studies/src/components/Cronometro/index.tsx
+++ alura-studies/src/components/Cronometro/index.tsx
@@ -1,53 +1,76 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
-import style from './Cronometro.module.scss';
-import {tempoParaSegundos} from '../../common/utils/time';
+import style from "./Cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/time";
 import { Itarefa } from "../../types/tarefa";
 import { SetStateAction, useEffect, useState } from "react";
 
-
 interface Props {
-    continua : boolean,
-    selecionado : Itarefa | undefined,
-    finalizarTarefa: () => void
-    
+  continua: boolean;
+  selecionado: Itarefa | undefined;
+  finalizarTarefa: () => void;
 }
 
-export default function Cronometro1({selecionado, finalizarTarefa, continua}: Props){
-    const [tempo, setTempo] = useState<number>();
-    const [continuaContagem, setcontinuaContagem] = useState<boolean>();
-    useEffect(() => {
-        if(selecionado?.tempo){
-            setTempo(tempoParaSegundos(selecionado.tempo))
-        }
-    },[selecionado]);
-
-    function regressiva(contador: number = 0, continuaContagem : boolean){
-            setTimeout(() => {
-                if(continuaContagem && contador > 0 ) {
-                    setTempo(contador - 1);
-                    return regressiva(contador - 1, continuaContagem);
-                    }else setcontinuaContagem(false); 
-                if(contador === 0) finalizarTarefa();
-                
-            }, 1000);
+export default function Cronometro1({
+  selecionado,
+  finalizarTarefa,
+  continua,
+}: Props) {
+  const [tempo, setTempo] = useState<number>();
+  const [iniciar, setIniciar] = useState<boolean>(false);
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+      setIniciar(false);
     }
+  }, [selecionado]);
 
-    return (
-        <div className={style.cronometro}>
-            <p className={style.titulo}>Escolha um card e inicie o cronometrô</p>
-            Tempo:{tempo}
-            
-            <div className={style.relogioWrapper}>
-                <Relogio tempo={tempo} />
-            </div>
-            <Botao onClick={() => regressiva(tempo, true)}>
-                Começar!
-            </Botao>
-            <Botao onClick={() => regressiva(tempo, false)}>
-                Parar!
-            </Botao>
-            
-        </div>
-    )
+  function pararCronometro(contador: number = 0) {
+    console.log("Apertado", iniciar);
+    setIniciar(false);
+  }
+
+  function iniciarCronometro(contador: number = 0) {
+    setTempo(contador - 1);
+    setIniciar(true);
+  }
+  useEffect(() => {
+    if (tempo && iniciar) {
+      setTimeout(contador, 1000);
+    }
+  }, [tempo, iniciar]);
+
+  function contador() {
+    if (tempo) {
+      const novoTempo = tempo - 1;
+      setTempo(novoTempo);
+      if (novoTempo == 0) {
+        finalizarTarefa();
+      }
+    }
+  }
+
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o cronometrô</p>
+      Tempo:{tempo}
+      <div className={style.relogioWrapper}>
+        <Relogio tempo={tempo} />
+      </div>
+      <Botao
+        onClick={() => {
+          iniciarCronometro(tempo);
+        }}
+      >
+        Começar!
+      </Botao>
+      <Botao
+        onClick={() => {
+          pararCronometro(tempo);
+        }}
+      >
+        Parar!
+      </Botao>
+    </div>
+  );
 }
