42237e84b90443d68edbb4a231c5e4f7655d26f0
RaulAnselmoDSantos <raul.trabalho@gmail.com>
2024-01-08 15:35:25 -0300

    feat: removing the mockdata

--- alura-studies/src/pages/App.tsx
+++ alura-studies/src/pages/App.tsx
@@ -4,12 +4,11 @@ import Lista from '../components/Lista';
 import style from './App.module.scss';
 import Cronometro from '../components/Cronometro';
 import { Itarefa } from '../types/tarefa';
-import { mockData } from '../types/tarefa';
 
 
 function App() {
 
-  const [tarefas, setTarefas] = useState<Itarefa[]>(mockData);
+  const [tarefas, setTarefas] = useState<Itarefa[]>([]);
   const [selecionado, setSelecionado] = useState<Itarefa>();
 
   function selecionaTarefa(tarefaSelecionada: Itarefa){
--- alura-studies/src/types/tarefa.ts
+++ alura-studies/src/types/tarefa.ts
@@ -7,28 +7,3 @@ export interface Itarefa {
     completado : boolean,
     id: string
   }
-
-  export const mockData = [
-    {
-      tarefa: 'React',
-      tempo: '02:00:00',
-      selecionado: false,
-      completado: false,
-      id: uuidv4()
-    }, 
-    {
-      tarefa: 'Javascript',
-      tempo: '01:00:00',
-      selecionado: false,
-      completado: false,
-      id: uuidv4()
-    }, 
-    {
-      tarefa: 'Typescript',
-      tempo: '03:00:00',
-      selecionado: false,
-      completado: false,
-      id: uuidv4()
-    }
-   ];
-  
